9716e01bf618cebe112c350f8c75fc1e0078866f
Dorin Botan <dorinbotan@mail.ru>
2018-10-04 18:54:53 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+https://stackoverflow.com/jobs/64810/do-you-have-what-it-takes-to-save-the-pony-take-trustpilot?med=clc
+https://ponychallenge.trustpilot.com/api-docs/index.html#/
---
+++ main.js
@@ -0,0 +1,147 @@
+#!/usr/bin/nodejs
+var request = require('request');
+
+const WIDTH = 15;
+const HEIGHT = 15;
+const DIFFICULTY = 1;
+
+(function createMaze(width, height, difficulty) {
+    request({
+        url: 'https://ponychallenge.trustpilot.com/pony-challenge/maze',
+        method: 'POST',
+        json: true,
+        body: {
+            'maze-width': width,
+            'maze-height': height,
+            'maze-player-name': 'applejack',
+            'difficulty': difficulty
+        }
+    }, (error, response, body) => {
+        if(!error && body.maze_id) {
+            getMaze(body.maze_id);
+        }
+    });
+})(WIDTH, HEIGHT, DIFFICULTY);
+
+function getMaze(maze_id) {
+    request({
+        url: 'https://ponychallenge.trustpilot.com/pony-challenge/maze/' + maze_id,
+        method: 'GET'
+    }, (error, response, body) => {
+        processMaze(maze_id,
+                    JSON.parse(body).pony,
+                    JSON.parse(body).domokun,
+                    JSON.parse(body)['end-point'],
+                    JSON.parse(body).data,
+                    JSON.parse(body).size);
+    });
+}
+
+function move(maze_id, direction) {
+    request({
+        url: 'https://ponychallenge.trustpilot.com/pony-challenge/maze/' + maze_id,
+        method: 'POST',
+        json: true,
+        body: {
+            'direction': direction,
+        }
+    }, (error, response, body) => {
+        if(!error && body.maze_id) {
+            getMaze(body.maze_id);
+        }
+    });
+}
+
+function printMaze(maze_id) {
+    request({
+        url: 'https://ponychallenge.trustpilot.com/pony-challenge/maze/' + maze_id + '/print',
+        method: 'GET'
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+function processMaze(maze_id, pony, domokun, end, walls, size) {
+    function Node(left, right, up, down) {
+        this.left = left;
+        this.right = right;
+        this.up = up;
+        this.down = down;
+    };
+
+    var getNode = (() => {
+        var nodes = [];
+
+        return (id) => { return nodes[id] || (nodes[id] = new Node()) };
+    })();
+
+    for(var i = 0; i < size[0] * size[1]; i++) {
+        if(!walls[i].includes('west')) {
+            getNode(i).left = getNode(i - 1);
+            getNode(i - 1).right = getNode(i);
+        }
+
+        if(!walls[i].includes('north')) {
+            getNode(i).up = getNode(i - size[0]);
+            getNode(i - size[1]).down = getNode(i);
+        }
+    }
+
+
+
+    function DirectedNode(parent, children) {
+        this.parent = parent;
+        this.children = children;
+    }
+
+    var getDirectedNode = (() => {
+        var nodes = [];
+
+        return (id) => { return nodes[id] || (nodes[id] = new DirectedNode()) };
+    })();
+
+    (function convert(node_id, parent_id) {
+        var currentNode = getNode(node_id);
+        getDirectedNode(node_id).parent = getDirectedNode(parent_id);
+
+        if(currentNode.left && currentNode.left.id != parent_id) {
+            getDirectedNode(node_id).children.push(getDirectedNode(currentNode.left.id));
+            convert(currentNode.left.id, node_id);
+        }
+
+        if(currentNode.right && currentNode.right.id != parent_id) {
+            getDirectedNode(node_id).children.push(getDirectedNode(currentNode.right.id));
+            convert(currentNode.right.id, node_id);
+        }
+
+        if(currentNode.up && currentNode.up.id != parent_id) {
+            getDirectedNode(node_id).children.push(getDirectedNode(currentNode.up.id));
+            convert(currentNode.up.id, node_id);
+        }
+
+        if(currentNode.down && currentNode.down.id != parent_id) {
+            getDirectedNode(node_id).children.push(getDirectedNode(currentNode.down.id));
+            convert(currentNode.down.id, node_id);
+        }
+    })(pony);
+
+//    move(maze_id, 'direction');
+}
+
+
+
+/******************************************************************************/
+/*                             [ ]                                            */
+/*                             ||                                             */
+/*                             V                                              */
+/*                        createMaze()                                        */
+/*                             ||                                             */
+/*                             V                                              */
+/*                          getMaze() <=====\\                                */
+/*                             ||           ||                                */
+/*         (goal reached)      V            ||                                */
+/*    [X] <============== processMaze()     ||                                */
+/*                             ||           ||                                */
+/*                             V            ||                                */
+/*                           move() ========//                                */
+/******************************************************************************/
